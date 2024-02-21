038b7b861eea59d150e21bdad0041321a7a44483
Matías <87877203+Matu10100@users.noreply.github.com>
2024-02-20 19:46:22 -0500

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,68 +1,70 @@
-import torch
-import torchvision
-from torch.utils.data import TensorDataset
-
+import warnings
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.exceptions import ConvergenceWarning
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
+ 
 #testing
 import os
 import argparse
 import wandb
-
+ 
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
 args = parser.parse_args()
-
+ 
 if args.IdExecution:
     print(f"IdExecution: {args.IdExecution}")
 else:
     args.IdExecution = "testing console"
-
-def preprocess(dataset, normalize=True, expand_dims=True):
+ 
+def preprocess(dataset, normalize=True):
     """
     ## Prepare the data
     """
-    x, y = dataset.tensors
-
+    X = dataset.drop('target', axis=1)
+    y = dataset['target']
     if normalize:
-        # Scale images to the [0, 1] range
-        x = x.type(torch.float32) / 255
-
-    if expand_dims:
-        # Make sure images have shape (1, 28, 28)
-        x = torch.unsqueeze(x, 1)
-    
-    return TensorDataset(x, y)
-
+        X_normalized = (X - X.min()) / (X.max() - X.min())  # Min-Max scaling
+        X = pd.DataFrame(X_normalized, columns=X.columns)  # Convert back to DataFrame
+ 
+        dataset = pd.concat([X,y], axis=1)
+    return dataset
+ 
 def preprocess_and_log(steps):
-
+ 
     with wandb.init(project="maestria_datos",name=f"Preprocess Data ExecId-{args.IdExecution}", job_type="preprocess-data") as run:    
         processed_data = wandb.Artifact(
-            "mnist-preprocess", type="dataset",
-            description="Preprocessed MNIST dataset",
+            "Housing-preprocess", type="dataset",
+            description="Preprocessed California Housing dataset",
             metadata=steps)
-         
         # ✔️ declare which artifact we'll be using
-        raw_data_artifact = run.use_artifact('mnist-raw:latest')
-
+        raw_data_artifact = run.use_artifact('Housing-Raw:latest')
+ 
         # 📥 if need be, download the artifact
         raw_dataset = raw_data_artifact.download(root="./data/artifacts/")
-        
-        for split in ["training", "validation", "test"]:
+        for split in ["training.table", "validation.table", "test.table"]:
             raw_split = read(raw_dataset, split)
             processed_dataset = preprocess(raw_split, **steps)
-
-            with processed_data.new_file(split + ".pt", mode="wb") as file:
-                x, y = processed_dataset.tensors
-                torch.save((x, y), file)
-
+ 
+            # Convert DataFrame to JSON and save
+            processed_data_json_path = f"./preprocessed_data/{split}.json"
+            processed_dataset.to_json(processed_data_json_path, orient="records")
+            # Log JSON file as an artifact
+            processed_data.add_file(processed_data_json_path, name=f"{split}.table")
+ 
         run.log_artifact(processed_data)
-
+ 
 def read(data_dir, split):
-    filename = split + ".pt"
-    x, y = torch.load(os.path.join(data_dir, filename))
-
-    return TensorDataset(x, y)
-
-steps = {"normalize": True,
-         "expand_dims": False}
-
+    filename = split + ".json"
+    file_path = os.path.join(data_dir, filename)
+    table = wandb.Table.load_from_file(file_path)
+    data = table.to_dataframe()
+    return data
+ 
+steps = {"normalize": True}
+df_names = ['training', 'validation', 'test']
+ 
 preprocess_and_log(steps)
