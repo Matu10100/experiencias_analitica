755c90b84e15a552975f54aac6552023e50a6651
Matías <87877203+Matu10100@users.noreply.github.com>
2024-02-20 20:33:54 -0500

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -60,7 +60,7 @@ def preprocess_and_log(steps):
 def read(data_dir, split):
     filename = split + ".json"
     file_path = os.path.join(data_dir, filename)
-    table = Housing_Raw.get(filename)
+    table = raw_data_artifact.get(filename)
     data = table.to_dataframe()
     return data
  
