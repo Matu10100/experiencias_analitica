25a7b1fec51d01d6acd287b6db020d818e25fb66
Matías <87877203+Matu10100@users.noreply.github.com>
2024-02-20 20:29:52 -0500

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -60,7 +60,7 @@ def preprocess_and_log(steps):
 def read(data_dir, split):
     filename = split + ".json"
     file_path = os.path.join(data_dir, filename)
-    table = wandb.Table(data=wandb.load(file_path))
+    table = Housing_Raw.get(filename)
     data = table.to_dataframe()
     return data
  
