63583ab837bcac822bcf656aa0733a54ec32fdd1
Matías <87877203+Matu10100@users.noreply.github.com>
2024-02-24 20:22:29 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -2,50 +2,50 @@ import os
 import argparse
 import wandb
 import warnings
-
+ 
 import numpy as np
 import pandas as pd
 import pickle
-
+ 
 from sklearn.linear_model import LinearRegression
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.exceptions import ConvergenceWarning
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
-
+ 
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
 args = parser.parse_args()
-
+ 
 if args.IdExecution:
     print(f"IdExecution: {args.IdExecution}")
 else:
     args.IdExecution = "testing console"
-
+ 
 def read(data_dir, filename):
     run = wandb.init()
     artifact = run.use_artifact('Housing-preprocess:latest')
     table = artifact.get(filename)
     data = table.get_dataframe()
     return data
-
+ 
 def train(config, dataset):
     X_train = dataset.drop('target', axis=1)
     y_train = dataset['target']
     # Train model, get predictions
-    reg = LinearRegression(config)
+    reg = LinearRegression(**config)
     model = reg.fit(X_train, y_train)
     return model
-
+ 
 def evaluate(dataset, model):
     X_test = dataset.drop('target', axis=1)
     y_test = dataset['target']
-
+ 
     y_pred = model.predict(X_test)
     r2 = metrics.r2_score(y_test, y_pred)
     MSE = metrics.mean_squared_error(y_test, y_pred)
     return r2, MSE, y_pred
-
+ 
 def train_and_log(config, experiment_id='99'):
     with wandb.init(
         project="maestria_datos", 
@@ -54,53 +54,43 @@ def train_and_log(config, experiment_id='99'):
         config = wandb.config
         data = run.use_artifact('Housing-preprocess:latest')
         data_dir = data.download(root="./data/artifacts/")
-
+ 
         training_set =  read(data_dir, "training.table.json")
         #validation_dataset = read(data_dir, "validation")
-        
         trained_model = train(config=config, dataset=training_set)
-
+ 
          # Save and log the trained model
-            # Contador para agregar un número secuencial al nombre del archivo
-    contador = 1
-    # Bucle para guardar varios modelos con nombres diferentes
-    for modelo in range(2):
-        # Nombre del archivo con un número secuencial
-        model_filename = f"trained_model_{contador}.pkl"
+        model_filename = "trained_model.pkl"
         with open(model_filename, 'wb') as model_file:
             pickle.dump(trained_model, model_file)
-        # Incrementar el contador para el siguiente modelo
-        contador += 1
-
+ 
         model_artifact = wandb.Artifact(
             "trained-model", type="model",
-            description="Trained Linear Regression model")
+            description="Trained Ridge model")
         model_artifact.add_file(model_filename)
         run.log_artifact(model_artifact)
-
+ 
     return trained_model
-
+ 
 def evaluate_and_log(model, experiment_id='99'):
-    
     with wandb.init(
         project="maestria_datos", name=f"Eval Model ExecId-{args.IdExecution} ExperimentId-{experiment_id}", 
         job_type="eval-model") as run:
-
+ 
         data = run.use_artifact('Housing-preprocess:latest')
         data_dir = data.download(root="./data/artifacts/")
         testing_set = read(data_dir, "test.table.json")
-
+ 
         # Evaluate the model
         r2, MSE, y_pred = evaluate(testing_set, model)
         wandb.log({"Test R^2": r2, "Test MSE": MSE})
-    
     return r2, MSE, y_pred
-
+ 
 # Main script
 parameters = ['True','False']
 for id,parameter in enumerate(parameters):
     # Train the model and get the trained model object
     config = {"positive" : parameter}
-    trained_model = train_and_log(config)
+    trained_model = train_and_log(config,id)
     # Evaluate the trained model and log evaluation metrics
-    evaluate_and_log(trained_model)
+    evaluate_and_log(id)
